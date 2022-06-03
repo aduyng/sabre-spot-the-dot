a5661e7bb84aa8d7e65e1894478fe24ee5ab813d
Duy Nguyen <aduyng@gmail.com>
2022-06-03 16:24:00 -0500

    image diffing improvements

--- api/src/bs/Screenshot/processImageDiff.js
+++ api/src/bs/Screenshot/processImageDiff.js
@@ -4,7 +4,8 @@ const {
   SCREENSHOT_DIR,
   RESEMBLE_OPTIONS,
   SCREENSHOT_STATUS_PROCESSING,
-  SCREENSHOT_STATUS_COMPLETE
+  SCREENSHOT_STATUS_COMPLETE,
+  UPLOAD_DIR
 } = require("../../consts");
 const getBucket = require("../../firebase/getBucket");
 const getScreenshot = require("./processImageDiff/getScreenshot");
@@ -13,8 +14,9 @@ const getScreenshotContent = require("./getScreenshotContent");
 
 module.exports = async ({ knex, bucket, name, contentType }) => {
   console.log(`[${__filename}] bucket: ${bucket}, name: ${name}, contentType: ${contentType}`);
+  const [projectId, jobId, launchId, screenshotId] = name.replace(`${UPLOAD_DIR}/`, "").split("/");
   const gcsBucket = getBucket();
-  const screenshot = await getScreenshot({ knex, name });
+  const screenshot = await getScreenshot({ knex, screenshotId });
   await knex("Screenshot")
     .update({
       status: SCREENSHOT_STATUS_PROCESSING,
@@ -27,7 +29,7 @@ module.exports = async ({ knex, bucket, name, contentType }) => {
     .first();
 
   if (launch.isGolden) {
-    const destination = `${SCREENSHOT_DIR}/${screenshot.id}-${screenshot.name}`;
+    const destination = `${SCREENSHOT_DIR}/${projectId}/${jobId}/${launchId}/${screenshot.id}/${screenshot.name}`;
     console.log(
       `the launch id: ${launch.id} is the golden launch, moving ${name} to ${destination}`
     );
@@ -38,13 +40,19 @@ module.exports = async ({ knex, bucket, name, contentType }) => {
     console.log(`the base screenshot of ${screenshot.id} is not found, this is a new screenshot`);
     return false;
   }
-  const destination = `${SCREENSHOT_DIR}/${screenshot.id}-${screenshot.name}`;
+  const destination = `${SCREENSHOT_DIR}/${projectId}/${jobId}/${launchId}/${screenshot.id}/${screenshot.name}`;
   await gcsBucket.file(name).move(destination);
   console.log(`the screenshot ${name} has been moved to ${destination}`);
 
   const [screenshotContent, baseScreenshotContent] = await Promise.all([
-    getScreenshotContent({ screenshot, throwError: true }),
-    getScreenshotContent({ screenshot: baseScreenshot, throwError: true })
+    getScreenshotContent({ projectId, jobId, launchId, screenshot, throwError: true }),
+    getScreenshotContent({
+      projectId,
+      jobId,
+      launchId,
+      screenshot: baseScreenshot,
+      throwError: true
+    })
   ]);
 
   console.log(`got the base and the screenshot, start the comparison`);
@@ -58,11 +66,11 @@ module.exports = async ({ knex, bucket, name, contentType }) => {
   const fileNameParts = screenshot.name.split(".");
   const extension = fileNameParts.pop();
   const diffFileName = `${fileNameParts.join(".")}.diff.${extension}`;
-  const diffFilePath = `${SCREENSHOT_DIR}/${screenshot.id}-${diffFileName}`;
+  const diffFilePath = `/${projectId}/${jobId}/${launchId}/${screenshot.id}/${diffFileName}`;
   await getBucket()
     .file(diffFilePath)
     .save(compareResult.getBuffer());
-  const diffPercentage = Math.round(compareResult.rawMisMatchPercentage * 100);
+  const diffPercentage = Math.round(parseFloat(compareResult.rawMisMatchPercentage) * 100);
   console.log(`diff file has been saved at ${diffFilePath}, diffPercentage: ${diffPercentage}`);
   await knex("Screenshot")
     .update({
