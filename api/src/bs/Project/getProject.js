41c7c7848845b45709cf23dc8e26b47a3b787eab
Dewey Nguyen <duy.nguyen@sabre.com>
2022-06-05 20:04:35 -0500

    lint fixes

--- api/src/bs/Project/getProject.js
+++ api/src/bs/Project/getProject.js
@@ -2,9 +2,9 @@ const { connectDb } = require("../../knex/knex");
 
 module.exports = async ({ userId, projectId }) => {
   const knex = await connectDb();
-  const res = await knex("Project")
+  return knex("Project")
     .column("Project.*")
     .innerJoin("UserProjectRole", "Project.id", "UserProjectRole.projectId")
-    .where({ "UserProjectRole.userId": userId, "Project.id": projectId }).first();
-    return res
+    .where({ "UserProjectRole.userId": userId, "Project.id": projectId })
+    .first();
 };
