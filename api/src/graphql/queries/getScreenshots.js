13d0b9077397a3a727f35efc33951a1ac166a5a7
Richard Kirk <richard.kirk@sabre.com>
2022-06-05 23:30:06 -0500

    lint

--- api/src/graphql/queries/getScreenshots.js
+++ api/src/graphql/queries/getScreenshots.js
@@ -1,4 +1,4 @@
-const getScreenshots = require("../../bs/Screenshot/getScreenshots")
+const getScreenshots = require("../../bs/Screenshot/getScreenshots");
 
 module.exports = (root, { launchId }, { user }) => {
   return getScreenshots({ launchId, userId: user.id });
